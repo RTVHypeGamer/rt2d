eef6135286e3fa0b2d66261fb930cb9bad8efde1
RTVHypeGamer <114234122+RTVHypeGamer@users.noreply.github.com>
2024-01-31 13:04:29 +0100

    indentations

--- PixelColor/pixelcolorscene.cpp
+++ PixelColor/pixelcolorscene.cpp
@@ -13,15 +13,15 @@ PixelColorScene::PixelColorScene() : SuperScene()
 	layers[0]->addChild(grid);
 
 	int counter = 0;
-	for (int x=0; x<gridwidth; x++) {
-		for (int y=0; y<gridheight ; y++) {
-			grid->spritebatch()[counter]->frame(rand()%16);
+	for (int x = 0; x < gridwidth; x++)
+	{
+		for (int y = 0; y < gridheight; y++)
+		{
+			grid->spritebatch()[counter]->frame(rand() % 16);
 			counter++;
 		}
 	}
-
 }
-
 
 PixelColorScene::~PixelColorScene()
 {
@@ -34,28 +34,34 @@ void PixelColorScene::update(float deltaTime)
 	SuperScene::update(deltaTime);
 	SuperScene::Camera(deltaTime);
 
-	int mousex = input()->getMouseX() + camera()->position.x - SWIDTH/2;
-	int mousey = input()->getMouseY() + camera()->position.y - SHEIGHT/2;
+	int mousex = input()->getMouseX() + camera()->position.x - SWIDTH / 2;
+	int mousey = input()->getMouseY() + camera()->position.y - SHEIGHT / 2;
 
-	std::vector<Sprite*> spritebatch = grid->spritebatch();
+	std::vector<Sprite *> spritebatch = grid->spritebatch();
 	int counter = 0;
-	for (int x=0; x<gridwidth; x++) {
-		for (int y=0; y<gridheight ; y++) {
+	for (int x = 0; x < gridwidth; x++)
+	{
+		for (int y = 0; y < gridheight; y++)
+		{
 			Point2 pos = spritebatch[counter]->spriteposition;
 
-			int halfwidth = cellwidth/2;
-			int halfheight = cellheight/2;
+			int halfwidth = cellwidth / 2;
+			int halfheight = cellheight / 2;
 			int left = pos.x - halfwidth;
 			int right = pos.x + halfwidth;
 			int top = pos.y - halfheight;
 			int bottom = pos.y + halfheight;
 
-			if ( mousex > left && mousex < right && mousey > top && mousey < bottom ) {
+			if (mousex > left && mousex < right && mousey > top && mousey < bottom)
+			{
 				spritebatch[counter]->color.a = 192;
-				if (input()->getMouseDown( 0 )) {
-					spritebatch[counter]->frame(rand()%16);
+				if (input()->getMouseDown(0))
+				{
+					spritebatch[counter]->frame(rand() % 16);
 				}
-			} else {
+			}
+			else
+			{
 				spritebatch[counter]->color.a = 255;
 			}
 			counter++;
